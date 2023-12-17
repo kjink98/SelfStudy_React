120c1b05fb93ec8805da4d5b179653cf8d9e6395
kjink98 <kjink98@naver.com>
2023-12-17 18:25:13 +0900

    리엑트 delete, Router, Link

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -62,6 +62,8 @@ function Create(props) {
 }
 
 function Update(props) {
+  const [title, setTitle] = useState(props.title);
+  const [body, setBody] = useState(props.body);
   return (
     <article>
       <h2>Update</h2>
@@ -71,8 +73,12 @@ function Update(props) {
         const body = event.target.body.value;
         props.onUpdate(title, body);
       }}>
-        <p><input type="text" name="title" placeholder='title' value={props.title} /></p>
-        <p><textarea name="body" placeholder='body' value={props.body}></textarea></p>
+        <p><input type="text" name="title" placeholder='title' value={title} onChange={event=>{
+          setTitle(event.target.value);
+          }}/></p>
+        <p><textarea name="body" placeholder='body' value={body} onChange={event=>{
+          setBody(event.target.value);
+        }}></textarea></p>
         <p><input type='submit' value="Update"></input></p>
       </form>
     </article>
@@ -103,10 +109,22 @@ function App() {
       }
     }
     content = <Article title={title} body={body}></Article>
-    contextControl = <li><a href={"/update/" + id} onClick={event => {
-      event.preventDefault();
-      setMode('UPDATE');
-    }}>Update</a></li>
+    contextControl = <>
+      <li><a href={"/update/" + id} onClick={event => {
+        event.preventDefault();
+        setMode('UPDATE');
+      }}>Update</a></li>
+      <li><input type="button" value="Delete" onClick={()=>{
+        const newTopics = []
+        for (let i=0; i<topics.length; i++){
+          if(topics[i].id !== id){
+            newTopics.push(topics[i]);
+          }
+        }
+        setTopics(newTopics);
+        setMode('WELCOME');
+      }} /></li>
+    </>
   } else if (mode === "CREATE") {
     content = <Create onCreate={(_title, _body) => {
       const newTopic = { id: nextId, title: _title, body: _body }
@@ -126,7 +144,16 @@ function App() {
       }
     }
     content = <Update title={title} body={body} onUpdate={(title, body) => {
-
+      const newTopics = [...topics]
+      const updateTopic = {id:id, title:title, body:body}
+      for (let i = 0; i < topics.length; i++) {
+        if (newTopics[i].id === id) {
+          newTopics[i] = updateTopic;
+          break;
+        }
+      }
+      setTopics(newTopics);
+      setMode('READ');
     }}></Update>
   }
 
